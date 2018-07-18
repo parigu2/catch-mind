fb7eb73d8cfc5aec3fa71a00c8fafbe573fd5c53
Thomas Kelly <tmkelly28@gmail.com>
2018-07-18 17:44:59 -0400

    lint, update dependencies and add readme

--- client/whiteboard.js
+++ client/whiteboard.js
@@ -14,7 +14,7 @@ const whiteboard = new EventEmitter()
 
 export default whiteboard
 
-//// Canvas setup
+// Canvas setup
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
 
@@ -26,139 +26,138 @@ const ctx = canvas.getContext('2d')
  * @param {String} strokeColor color of the line
  * @param {bool} shouldBroadcast whether to emit an event for this draw
  */
-export function draw(start, end, strokeColor='black', shouldBroadcast=true) {
-    // Draw the line between the start and end positions
-    // that is colored with the given color.
-    ctx.beginPath();
-    ctx.strokeStyle = strokeColor;
-    ctx.moveTo(...start);
-    ctx.lineTo(...end);
-    ctx.closePath();
-    ctx.stroke();
+export function draw (start, end, strokeColor = 'black', shouldBroadcast = true) {
+  // Draw the line between the start and end positions
+  // that is colored with the given color.
+  ctx.beginPath()
+  ctx.strokeStyle = strokeColor
+  ctx.moveTo(...start)
+  ctx.lineTo(...end)
+  ctx.closePath()
+  ctx.stroke()
 
-    // If shouldBroadcast is truthy, we will emit a draw event to listeners
-    // with the start, end and color data.
-    shouldBroadcast &&
-        whiteboard.emit('draw', start, end, strokeColor);
-};
+  // If shouldBroadcast is truthy, we will emit a draw event to listeners
+  // with the start, end and color data.
+  shouldBroadcast &&
+        whiteboard.emit('draw', start, end, strokeColor)
+}
 
 // State
-//// Stroke color
-let color;
-//// Position tracking
+/// / Stroke color
+let color
+/// / Position tracking
 let currentMousePosition = {
-    x: 0,
-    y: 0
-};
+  x: 0,
+  y: 0
+}
 
 let lastMousePosition = {
-    x: 0,
-    y: 0
-};
+  x: 0,
+  y: 0
+}
 
-//// Color picker settings
+/// / Color picker settings
 const colors = [
-    'black',
-    'purple',
-    'red',
-    'green',
-    'orange',
-    'yellow',
-    'brown',
+  'black',
+  'purple',
+  'red',
+  'green',
+  'orange',
+  'yellow',
+  'brown'
 ]
 
-function setup() {
-    document.body.appendChild(canvas)
+function setup () {
+  document.body.appendChild(canvas)
 
-    setupColorPicker()
-    setupCanvas()
+  setupColorPicker()
+  setupCanvas()
 }
 
-function setupColorPicker() {
-    const picker = document.createElement('div')
-    picker.classList.add('color-selector')
-    colors
-        .map(color => {
-            const marker = document.createElement('div')
-            marker.classList.add('marker')
-            marker.dataset.color = color
-            marker.style.backgroundColor = color
-            return marker
-        })
-        .forEach(color => picker.appendChild(color))
-
-    picker.addEventListener('click', ({target}) => {
-        color = target.dataset.color
-        if (!color) return
-        const current = picker.querySelector('.selected')
-        current && current.classList.remove('selected')
-        target.classList.add('selected')
+function setupColorPicker () {
+  const picker = document.createElement('div')
+  picker.classList.add('color-selector')
+  colors
+    .map(color => {
+      const marker = document.createElement('div')
+      marker.classList.add('marker')
+      marker.dataset.color = color
+      marker.style.backgroundColor = color
+      return marker
     })
+    .forEach(color => picker.appendChild(color))
 
-    document.body.appendChild(picker)
+  picker.addEventListener('click', ({target}) => {
+    color = target.dataset.color
+    if (!color) return
+    const current = picker.querySelector('.selected')
+    current && current.classList.remove('selected')
+    target.classList.add('selected')
+  })
 
-    // Select the first color
-    picker.firstChild.click()
+  document.body.appendChild(picker)
+
+  // Select the first color
+  picker.firstChild.click()
 }
 
+function resize () {
+  // Unscale the canvas (if it was previously scaled)
+  ctx.setTransform(1, 0, 0, 1, 0, 0)
 
-function resize() {
-    // Unscale the canvas (if it was previously scaled)
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
+  // The device pixel ratio is the multiplier between CSS pixels
+  // and device pixels
+  const pixelRatio = window.devicePixelRatio || 1
 
-    // The device pixel ratio is the multiplier between CSS pixels
-    // and device pixels
-    var pixelRatio = window.devicePixelRatio || 1;
+  // Allocate backing store large enough to give us a 1:1 device pixel
+  // to canvas pixel ratio.
+  const w = canvas.clientWidth * pixelRatio
+  const h = canvas.clientHeight * pixelRatio
+  if (w !== canvas.width || h !== canvas.height) {
+    // Resizing the canvas destroys the current content.
+    // So, save it...
+    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
-    // Allocate backing store large enough to give us a 1:1 device pixel
-    // to canvas pixel ratio.
-    var w = canvas.clientWidth * pixelRatio,
-        h = canvas.clientHeight * pixelRatio;
-    if (w !== canvas.width || h !== canvas.height) {
-        // Resizing the canvas destroys the current content.
-        // So, save it...
-        var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    canvas.width = w; canvas.height = h
 
-        canvas.width = w; canvas.height = h;
+    // ...then restore it.
+    ctx.putImageData(imgData, 0, 0)
+  }
 
-        // ...then restore it.
-        ctx.putImageData(imgData, 0, 0)
-    }
+  // Scale the canvas' internal coordinate system by the device pixel
+  // ratio to ensure that 1 canvas unit = 1 css pixel, even though our
+  // backing store is larger.
+  ctx.scale(pixelRatio, pixelRatio)
 
-    // Scale the canvas' internal coordinate system by the device pixel
-    // ratio to ensure that 1 canvas unit = 1 css pixel, even though our
-    // backing store is larger.
-    ctx.scale(pixelRatio, pixelRatio);
-
-    ctx.lineWidth = 5
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
+  ctx.lineWidth = 5
+  ctx.lineJoin = 'round'
+  ctx.lineCap = 'round'
 }
 
-function setupCanvas() {
-    // Set the size of the canvas and attach a listener
-    // to handle resizing.
-    resize()
-    window.addEventListener('resize', resize)
+function setupCanvas () {
+  // Set the size of the canvas and attach a listener
+  // to handle resizing.
+  resize()
+  window.addEventListener('resize', resize)
 
-    window.addEventListener('mousedown', function (e) {
-        currentMousePosition = pos(e)
-    });
+  window.addEventListener('mousedown', (e) => {
+    currentMousePosition = pos(e)
+  })
 
-    window.addEventListener('mousemove', function (e) {
-        if (!e.buttons) return;
-        lastMousePosition = currentMousePosition
-        currentMousePosition = pos(e)
-        lastMousePosition && currentMousePosition &&
-            draw(lastMousePosition, currentMousePosition, color, true);
-    });
+  window.addEventListener('mousemove', (e) => {
+    if (!e.buttons) return
+    lastMousePosition = currentMousePosition
+    currentMousePosition = pos(e)
+    lastMousePosition && currentMousePosition &&
+            draw(lastMousePosition, currentMousePosition, color, true)
+  })
 }
 
-function pos(e) {
-    return [
-        e.pageX - canvas.offsetLeft,
-        e.pageY - canvas.offsetTop
-    ]
+function pos (e) {
+  return [
+    e.pageX - canvas.offsetLeft,
+    e.pageY - canvas.offsetTop
+  ]
 }
 
 document.addEventListener('DOMContentLoaded', setup)
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+const path = require('path')
+const express = require('express')
+const app = express()
 
 // app.listen() returns an http.Server object
 // http://expressjs.com/en/4x/api.html#app.listen
-const server = app.listen(1337, function () {
-    console.log(`Listening on http://localhost:${server.address().port}`);
-});
+const server = app.listen(1337, () => {
+  console.log(`Listening on http://localhost:${server.address().port}`)
+})
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
