137de7220eb0e09e565c7ed60860302a20c6b44b
Yang <minkyu.yang@capgemini.com>
2019-07-03 14:53:51 -0500

    login page

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express')
 const app = express()
 const socketio = require('socket.io');
 
-// app.listen() returns an http.Server object
-// http://expressjs.com/en/4x/api.html#app.listen
 const server = app.listen(1337, () => {
   console.log(`Listening on http://localhost:${server.address().port}`)
 })
