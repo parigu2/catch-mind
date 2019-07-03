f16e7c31526b79d73c9c631840470202a9446cfb
Yang <minkyu.yang@capgemini.com>
2019-07-03 13:43:56 -0500

    add main page

--- client/index.js
+++ client/index.js
@@ -3,5 +3,28 @@
 //   An export named `events`, calling it `whiteboard`.
 import whiteboard, {draw} from './whiteboard'
 
-// Example: Draw a single stroke.
-draw([0, 0], [250, 250], 'red', true)
+// import createClientSocket from 'socket.io.client';
+// const clientSocket = createClientSocket(window.location.origin);
+
+const socket = io(window.location.origin);
+const drawingName = window.location.pathname;
+
+socket.on('connect', () => {
+    console.log("I have made a persistent two-way connection to the server!");
+    socket.emit('join-drawing', drawingName);
+
+})
+
+socket.on('replay-drawing', instructions => {
+    instructions.forEach(instruction => {
+        draw(...instruction, false);
+    })
+})
+
+socket.on('drawReceived', (start, end, color) => {
+    draw(start, end, color, false);
+})
+
+whiteboard.on('draw', (start, end, color) => {
+    socket.emit('drawSend', drawingName, start, end, color);
+})
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const socketio = require('socket.io');
 
 // app.listen() returns an http.Server object
 // http://expressjs.com/en/4x/api.html#app.listen
@@ -8,4 +9,76 @@ const server = app.listen(1337, () => {
   console.log(`Listening on http://localhost:${server.address().port}`)
 })
 
+const io = socketio(server);
+
+const drawings = {};
+const connections = {};
+
+function getDrawing (drawingName) {
+  if (drawings[drawingName] === undefined) {
+    drawings[drawingName] = {
+      connection: 0,
+      pen: []
+    }
+  }
+  return drawings[drawingName];
+}
+
+function joinRoom(drawingName, socketId) {
+  connections[socketId] = drawingName
+  drawings[drawingName].connection++;
+}
+
+function leaveRoom (socketId) {
+  const drawingName = connections[socketId];
+  delete connections[socketId]
+  const remaining = --drawings[drawingName].connection;
+  if(!remaining) {
+    drawings[drawingName].pen = []
+  }
+}
+
+io.on('connection', socket => {
+  console.log('A new client has connected!');
+  console.log(socket.id);
+  
+  socket.on('join-drawing', drawingName => {
+    socket.join(drawingName);
+    const drawing = getDrawing(drawingName)
+    joinRoom(drawingName, socket.id)
+    socket.emit('replay-drawing', drawing.pen);
+  })
+
+  socket.on('drawSend', (drawingName, start, end, color) => {
+    const drawing = getDrawing(drawingName)
+    drawing.pen.push([start, end, color]);
+    // socket.broadcast.emit('drawReceived', start, end, color);
+    socket.broadcast.to(drawingName).emit('drawReceived', start, end, color);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected :<', socket.id);
+    leaveRoom(socket.id)
+    io.emit('user disconnected');
+  })
+})
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'public', 'main.html'));
+  console.log(drawings)
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/:number', (req, res, next) => {
+  const drawingName = `/${req.params.number}`
+  if(drawings[drawingName] !== undefined) {
+    if(drawings[drawingName].connection > 3) {
+      res.redirect('/')
+    } else {
+      res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    }
+  } else {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  }
+})
